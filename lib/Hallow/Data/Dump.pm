b1d089fe9a624c4dbf6f1da146e3a84acb1b9892
Toshinori Sato (@overlast) <overlasting@gmail.com>
2013-11-08 21:18:55 +0900

    Add a module which include utility functions to dump the data

---
+++ lib/Hallow/Data/Dump.pm
@@ -0,0 +1,41 @@
+package Hallow::Data::Dump;
+
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.0.0_01";
+
+use utf8;
+use autodie;
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Hallow::Data::Dump - It's new $module
+
+=head1 SYNOPSIS
+
+    use Hallow::Data::Dump;
+
+=head1 DESCRIPTION
+
+Hallow::Data::Dump is ...
+
+=head1 LICENSE
+
+Copyright (C) Toshinori Sato (@overlast).
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Toshinori Sato (@overlast) E<lt>overlasting@gmail.comE<gt>
+
+=cut
