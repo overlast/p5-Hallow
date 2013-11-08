caa74103fdc5b6b20aa00fbdd0157674763efa52
Toshinori Sato (@overlast) <overlasting@gmail.com>
2013-11-08 21:19:50 +0900

    Add a module which include utility functions to control the data handlers

---
+++ lib/Hallow/Data/Handle.pm
@@ -0,0 +1,41 @@
+package Hallow::Data::Handle;
+
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.0.0_01";
+
+use utf8;
+use autodie;
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Hallow::Data::Handle - It's new $module
+
+=head1 SYNOPSIS
+
+    use Hallow::Data::Handle;
+
+=head1 DESCRIPTION
+
+Hallow::Data::Handle is ...
+
+=head1 LICENSE
+
+Copyright (C) Toshinori Sato (@overlast).
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Toshinori Sato (@overlast) E<lt>overlasting@gmail.comE<gt>
+
+=cut
