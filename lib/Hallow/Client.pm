082f54c2999e1234426c29c601c012babf3a5677
Toshinori Sato (@overlast) <overlasting@gmail.com>
2013-12-04 00:07:27 +0900

    Add get_max_result_num() and get_ml_client()

--- lib/Hallow/Client.pm
+++ lib/Hallow/Client.pm
@@ -52,26 +52,65 @@ sub new {
 
 sub get_ml_client {
     my ($self, $module_name) = @_;
-    my $config = $self->{config};
-    my $recipe_name = $self->{recipe_name};
-    my $platform = $config->{$recipe_name}->{$module_name}->{platform};
     my $ml_client = "";
-    if ($platform eq "jubatus") {
-        my $jubatus_conf = $config->{platform}->{jubatus};
-        my $server_name = $jubatus_conf->{server_name};
-        my $host = $jubatus_conf->{host};
-        my $port = $jubatus_conf->{port};
-        $server_name =~ s|juba||;
-        $ml_client = Jubatus->get_client($host, $port, $server_name);
+    my $config = $self->{config};
+    if (exists $self->{recipe_name}) {
+        my $recipe_name = $self->{recipe_name};
+        if (defined $module_name) {
+            if (exists $config->{$recipe_name}->{$module_name}) {
+                if (exists $config->{$recipe_name}->{$module_name}->{platform}) {
+                    my $platform = $config->{$recipe_name}->{$module_name}->{platform};
+                    if ($platform =~ m|jubatus|i) {
+                        my $jubatus_conf = $config->{platform}->{jubatus};
+                        my $server_name = $jubatus_conf->{server_name};
+                        my $host = $jubatus_conf->{host};
+                        my $port = $jubatus_conf->{port};
+                        $server_name =~ s|juba||;
+                        $ml_client = Jubatus->get_client($host, $port, $server_name);
+                    } else {
+                        warnf "config->{$recipe_name}->{$module_name}->{platform} should be (|jubatus|i)" if (HALLOW_DEBUG);
+                    }
+                } else {
+                    warnf "config->{$recipe_name}->{$module_name}->{platform} should be define in a configure file" if (HALLOW_DEBUG);
+                }
+            } else {
+                warnf "config->{$recipe_name}->{module_name} should be define in a configure file" if (HALLOW_DEBUG);
+            }
+        } else {
+            warnf "First argument of get_ml_client() should be a module name" if (HALLOW_DEBUG);
+        }
+    } else {
+        warnf "self->{recipe_name} should be define in a constructor" if (HALLOW_DEBUG);
     }
     return $ml_client;
 }
 
 sub get_max_result_num {
     my ($self, $module_name) = @_;
+    # 設定ファイルから件数を読めない時は10と返す
     my $max_result_num = 10; #default
     my $config = $self->{config};
-    my $recipe_name = $self->{recipe_name};
-    $max_result_num = $config->{$recipe_name}->{$module_name}->{max_result_num} if (exists $config->{$recipe_name}->{$module_name}->{max_result_num});
+    # recipe_name がHallow::Client以下のnew()で指定されている必要がある
+    if (exists $self->{recipe_name}) {
+        my $recipe_name = $self->{recipe_name};
+        # module_nameが定義されている必要がある
+        if (defined $module_name) {
+            # config->{$recipe_name}->{$module_name}が定義されている必要がある
+            if (exists $config->{$recipe_name}->{$module_name}) {
+                # max_result_num で設定ファイルから件数を読み込む
+                if (exists $config->{$recipe_name}->{$module_name}->{max_result_num}) {
+                    $max_result_num = $config->{$recipe_name}->{$module_name}->{max_result_num};
+                } else {
+                    warnf "config->{$recipe_name}->{$module_name}->{max_result_num} should be define in configure file" if (HALLOW_DEBUG);
+                }
+            } else {
+                warnf "First argument of get_max_result_num() is module_name" if (HALLOW_DEBUG);
+            }
+        } else {
+            warnf "config->{$recipe_name}->{module_name} should be define in configure file" if (HALLOW_DEBUG);
+        }
+    } else {
+        warnf "self->{recipe_name} should be define in constructor" if (HALLOW_DEBUG);
+    }
     return $max_result_num;
 }
